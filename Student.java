2e6812d058171c9879b6da8f09a29ad651d7c2bf
Rory Hackney <72114389+roryhackney@users.noreply.github.com>
2023-04-11 20:40:36 -0700

    JavaDoc, Comparable

--- Student.java
+++ Student.java
@@ -1,1 +1,14 @@
-public record Student(String id, String lastName, String firstName, String email, String phone) {}
+/**
+ * Manages student data
+ * @param id    id number assigned to the student, must not be null or blank
+ * @param lastName last name, must not be null or blank
+ * @param firstName first name, must not be null or blank
+ * @param email email contact, must not be null or blank
+ * @param phone phone number contact, must not be null or blank
+ */
+public record Student(String id, String lastName, String firstName, String email, String phone) implements Comparable<Student> {
+    @Override
+    public int compareTo(Student o) {
+        return 0;
+    }
+}
